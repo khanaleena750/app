32643ef08e97fd168059fe6233910cbd5d74b2bf
khanaleena750 <148544727+khanaleena750@users.noreply.github.com>
2024-02-23 15:03:40 +0400

    Add files via upload

---
+++ product.js
@@ -0,0 +1,82 @@
+let products = [
+    {
+              id: 1,
+              subject: "Maths",
+              location: "Russia",
+              price: 55,
+              spaces: 5,
+              image: "s1.png"
+            },
+            {
+              id: 2,
+              subject: "Drama",
+              location: "Spain",
+              price: 80,
+              spaces: 3,
+              image: "s2.png"
+            },
+              {
+                  id: 3,
+                  subject: "Accounting",
+                  location: "Germany",
+                  price: 50,
+                  spaces: 2,
+                  image: "s12.png" 
+                },
+              {
+                id: 4,
+                subject: "Journaling",
+                location: "Mexico",
+                price: 10,
+                spaces: 8,
+                image: "s11.png" 
+              },
+              {
+                id: 5,
+                subject: "Coding",
+                location: "London",
+                price: 50,
+                spaces: 7,
+                image: "s10.png"
+              },
+              {
+                id: 6,
+                subject: "Baking",
+                location: "Turkey",
+                price: 45,
+                spaces: 3,
+                image: "s9.png"  
+              },
+              {
+                id: 7,
+                subject: "Music",
+                location: "Azerbaijan",
+                price: 70,
+                spaces: 2,
+                image: "s8.png"
+              },
+              {
+                id: 8,
+                subject: "Psychology",
+                location: "Georgia",
+                price: 90,
+                spaces: 12,
+                image: "s7.png"
+              },
+              {
+                id: 9,
+                subject: "Business",
+                location: "Argentina",
+                price: 20,
+                spaces: 9,
+                image: "s6.png"
+              },
+              {
+                id: 10,
+                subject: "Art",
+                location: "Italy",
+                price: 50,
+                spaces: 6,
+                image: "s5.png"
+              },
+]

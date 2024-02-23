2b1c0ae7a0300d635db212e22e960daa16ae32a3
khanaleena750 <148544727+khanaleena750@users.noreply.github.com>
2024-02-23 15:55:17 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -26,14 +26,28 @@ self.addEventListener('install', (e) => {
         })
     );
 });
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         //check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: '
+//             + e.request.url);
+//             // 'r' is matching file if it exists in the cache
+//             return r
+//         })
+//     );
+// });
 self.addEventListener('fetch', function (e) {
     e.respondWith(
-        //check if the cache has the file
         caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resource: '
-            + e.request.url);
-            // 'r' is matching file if it exists in the cache
-            return r
+            // download the file if it is not in the cache
+            return r || fetch(e.request).then(function (response) {
+                //add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
